386265a64b873dfec76c281a84ebe63134e996e5
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-11-11 03:28:43 -0800

    updates

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,12 +3,19 @@ from .views import list_books
 from .views import LibraryDetailView 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
-from .import views
+from .views import register
+from . import views
 
 urlpatterns = [
-    path('register/' , views.register.as_view(), name = 'register'),
+    path('register/', views.register.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('library/', LibraryDetailView.as_view(), name='library-detail'),
     path('list_books/', list_books, name='list_books'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add/', views.add_book, name='add_book/'),
+    path('edit/<int:pk>/', views.edit_book, name='edit_book/'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book/'),
 ]
