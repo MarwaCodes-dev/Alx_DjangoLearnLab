e1b7c0f1f9deb45ed91c2e39fc95cfd069ed41c4
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-12-13 11:40:11 -0800

    new file

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import User 
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
+        return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid username or password.")
+        return user
+
+class TokenSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    profile_picture = serializers.ImageField(source='user.profile_picture', read_only=True)
+    bio = serializers.CharField(source='user.bio', read_only=True)
+
+    class Meta:
+        model = Token
+        fields = ['key', 'username', 'email', 'profile_picture', 'bio']
